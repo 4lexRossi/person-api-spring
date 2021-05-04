d351873ef46db2a4a3eb09c79335aedd92e70128
4lexRossi <devalexrossi@gmail.com>
2021-05-03 22:16:40 -0300

    fix post method

--- src/main/java/com/alos/persona/dto/mapper/PersonMapper.java
+++ src/main/java/com/alos/persona/dto/mapper/PersonMapper.java
@@ -4,12 +4,15 @@ import com.alos.persona.dto.request.PersonDTO;
 import com.alos.persona.entities.Person;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface PersonMapper {
 
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+
     @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
     Person toModel(PersonDTO dto);
 
-    PersonDTO toDTO(Person dto);
+    PersonDTO toDTO(Person person);
 }
