45dbf525ab0a5f17d88354e7b921de5e291c70a4
4lexRossi <devalexrossi@gmail.com>
2021-05-04 22:02:48 -0300

    unit tests

--- src/test/java/com/alos/persona/utils/PersonUtils.java
+++ src/test/java/com/alos/persona/utils/PersonUtils.java
@@ -1,5 +1,9 @@
 package com.alos.persona.utils;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.alos.persona.dto.request.PersonDTO;
 import com.alos.persona.entities.Person;
 
@@ -35,7 +39,17 @@ public class PersonUtils {
                 .build();
     }
 
+    public static String asJsonString(PersonDTO personDTO) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+            objectMapper.registerModules(new JavaTimeModule());
 
+            return objectMapper.writeValueAsString(personDTO);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
 
 }
